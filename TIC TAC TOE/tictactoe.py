c66bfdee0368ddc1f30ff7f834e7d7d580535077
Avirup Mazumder <117657367+exd35-avi@users.noreply.github.com>
2023-01-23 20:59:28 +0530

    Add files via upload

---
+++ TIC TAC TOE/tictactoe.py
@@ -0,0 +1,73 @@
+def printBoard(xState,zState):
+    zero = 'X' if xState[0] else ('O' if zState[0] else 0)
+    one = 'X' if xState[1] else ('O' if zState[1] else 1)
+    two = 'X' if xState[2] else ('O' if zState[2] else 2)
+    three = 'X' if xState[3] else ('O' if zState[3] else 3)
+    four = 'X' if xState[4] else ('O' if zState[4] else 4)
+    five = 'X' if xState[5] else ('O' if zState[5] else 5)
+    six = 'X' if xState[6] else ('O' if zState[6] else 6)
+    seven = 'X' if xState[7] else ('O' if zState[7] else 7)
+    eight = 'X' if xState[8] else ('O' if zState[8] else 8)
+    
+    print(f"{zero} | {one} | {two} ")
+    print(f"__| __| __")
+    print(f"{three} | {four} | {five} ")
+    print(f"__| __| __")
+    print(f"{six} | {seven} | {eight} ")
+
+def sum(a,b,c):
+    return (a+b+c)
+
+def checkWin(xState,zState):
+    wins = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    for win in wins:
+        if(sum(xState[win[0]], xState[win[1]], xState[win[2]]) == 3):
+            print('X won the match')
+            return 1
+        if(sum(zState[win[0]], zState[win[1]], zState[win[2]]) == 3):
+            print('O won the match')
+            return 1
+    return -1
+
+def main():
+    # should be square matrix
+    n = eval(input('Enter the no. of row (same as coloumn): '))
+    xState = []
+    zState = []
+
+    for i in range(n*n):
+        xState.append(0)
+        zState.append(0)
+
+    count = 0
+    turn = 1    # 1 for X and 0 for O
+    print("Welcome to the game")
+
+    while(True):
+        printBoard(xState,zState)
+        if(turn == 1):
+            print("'X's chance")
+            value = eval(input("Please enter a value "))
+            xState[value] = 1
+            count = count+1
+        else:
+            print("'O's chance")
+            value = eval(input("Please enter a value "))
+            zState[value] = 1
+            count = count+1
+
+        chk = checkWin(xState,zState)
+        if chk == 1:
+            printBoard(xState,zState)
+            print('Match Over')
+            return
+
+        turn = 1 - turn
+        if count == 9 and chk == -1:
+            printBoard(xState,zState)
+            print('Match Drawn')
+            return
+
+if __name__ == "__main__":
+    main()
+    
